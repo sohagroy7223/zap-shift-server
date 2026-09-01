723d6beb408c12c434c4e0771b25319f781e5663
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-01 13:36:07 +0600

    addad a sort option that letast products state in the top

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ async function connectToMongoDB() {
       if (email) {
         query.senderEmail = email;
       }
-      const cursor = parcelCollection.find(query);
+
+      const options = { sort: { createdAt: -1 } };
+
+      const cursor = parcelCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     });
