4dd4301c8757b7868a070800ec5ae5075f6cbf43
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-18 10:19:32 +0600

    addad a get method for get data in database a particular rider email data

--- index.js
+++ index.js
@@ -238,6 +238,20 @@ async function connectToMongoDB() {
       res.send(result);
     });
 
+    app.get("/parcels/rider", async (req, res) => {
+      const { riderEmail, deliveryStatus } = req.query;
+      const query = {};
+      if (riderEmail) {
+        query.riderEmail = riderEmail;
+      }
+      if (deliveryStatus) {
+        query.deliveryStatus = deliveryStatus;
+      }
+      const cursor = parcelCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
